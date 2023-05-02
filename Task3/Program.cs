51ed87804f225df05e7c67e60eb60ba65f72ef6c
Egor.S <Egorsoliakov@gmail.com>
2023-05-02 21:33:24 +0400

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Программа выдающая таблицу кубов от 1 до N
+
+
+int n = Prompt("Введите число N: ");
+
+NumbTable (n);
+
+void NumbTable(int n)
+{
+    if(n<0)
+    {
+        int M = -1;
+        while(n<=M)
+       {
+    
+         int sqr = n*n*n;
+        Console.WriteLine(sqr);
+         n++;
+       }
+    }
+    else
+    {
+    int S = 1;
+    
+while(S<=n)
+{
+    
+    int sqr = S*S*S;
+    Console.WriteLine(sqr);
+    S++;
+}
+}
+}
+
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    int coordNumber = int.Parse(Console.ReadLine());
+    int n = Convert.ToInt32(coordNumber);
+    return (n);
+}
